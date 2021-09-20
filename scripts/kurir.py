425620d1c39175a2651a23256dbb60b433e210b7
Nikola <anotherdevdzoni@gmail.com>
2021-09-20 12:26:53 +0200

    fix kurir scraper

--- scripts/kurir.py
+++ scripts/kurir.py
@@ -10,31 +10,27 @@ import sys, requests as reqs
 from bs4 import BeautifulSoup
 
 def main():
-    
-    kurir = 'https://www.kurir.rs'
-    
-    page = reqs.get(kurir)
-    page.raise_for_status()
-    
-    soup = BeautifulSoup(page.text, 'html.parser')
-    
-    all_news = soup.findAll('div',  {'class' : 'itemContent'})
-    
-    res = dict()
-    
-    for news in all_news:
-        link = news.a['href']
-        if link.find('www') == -1:
-            link = kurir + link
-        title = news.find('h2').getText().strip()
-        if title.find('\n') != -1:
-            title = title[:title.find('\n')].strip()
-        
-        res[title] = link
-    
-    return res
-                
+	
+	kurir = 'https://www.kurir.rs/rss'
+	
+	page = reqs.get(kurir)
+	page.raise_for_status()
+	
+	page = BeautifulSoup(page.text, 'xml')
+	
+	all_news = page.findAll('item')
+	
+	res = dict()
+	
+	for news in all_news:
+		link = news.link.getText()
+		title = news.title.getText()
+		
+		res[title] = link
+	
+	return res
+				
 if __name__ == '__main__':
-    news = main()
-    for k,v in news.items():
-        print(v)
+	news = main()
+	for k,v in news.items():
+		print(v)
